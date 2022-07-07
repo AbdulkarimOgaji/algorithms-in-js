1564b58f99074917a4d48b4b371fe0db9b908e50
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-07 15:05:48 +0100

    add miscellaneous

---
+++ misc/cartesian-product.js
@@ -0,0 +1,12 @@
+
+
+// O(nm)
+function cartesian(arr1, arr2) {
+    var result = []
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            result.push([arr1[i], arr2[j]])
+        }
+    }
+    return result
+}

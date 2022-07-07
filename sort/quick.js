7b061e5ba22d17dc22e22cb8731a5c23b7abb9b2
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-07 12:50:29 +0100

    quick sort

---
+++ sort/quick.js
@@ -0,0 +1,22 @@
+
+
+// Worst case - O(n^2) - when array is already sorted
+// Avg case - O(nlogn)
+function quick(arr) {
+    if (arr.length < 2) return arr
+    var left = []
+    var right = []
+    var pivot = arr[arr.length-1]
+
+    for (let i = 0; i < arr.length-1; i++) {
+        if (arr[i] < pivot) left.push(arr[i])
+        else right.push(arr[i])
+    }
+    return [...quick(left), pivot, ...quick(right)]
+}
+
+
+const array = [-6, 1, -7, 15, 14, 3, 2, 1]
+
+
+console.log(quick(array))
